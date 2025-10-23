93b1a3a5d53cf0beca2d1a1597bc000ba767a123
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-10-23 22:45:57 +0900

    Feat : カメラの動きを補完に

--- Assets/ChronosFall/Scripts/Characters/PlayerControl/Camera/CameraMove.cs
+++ Assets/ChronosFall/Scripts/Characters/PlayerControl/Camera/CameraMove.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ChronosFall.Scripts.Characters.PlayerControl.Camera
 {
     public class CameraMove : MonoBehaviour
     {
-        [Header("カメラ")] private float _sensitivity = 1.0f;
-        private float _maxLookAngleX = 55f;
-        [Header("カメラピボット")] [SerializeField] private GameObject _cameraPivot;
+        // カメラから見て上下 : X軸回転（ピッチ） | 左右 : Y軸回転（ヨー）
+        [Header("カメラ")] 
+        public float sensitivityX = 1.0f; // 感度X
+        public float sensitivityY = 1.0f; // 感度Y
+        public float smoothTime = 0.05f;
+        private float _maxLookAngleY = 55f; // カメラY座標軸の限度
+        
+        [Header("カメラピボット")]
+        private GameObject _cameraPivot;
         private bool _isLockCursor; // クリックのロック
 
         private float _currentX = 0f; // 現在の上下回転角度
         private float _currentY = 0f; // 現在の左右回転角度
+        // 補完用
+        private float _smoothX, _smoothY;
+        private float _velocityX, _velocityY;
+        
 
         private void Start()
         {
@@ -22,18 +33,18 @@ namespace ChronosFall.Scripts.Characters.PlayerControl.Camera
 
         private void Update()
         {
-            if (_maxLookAngleX <= 0f) _maxLookAngleX = 80f; // デフォルト値を設定
+            if (_maxLookAngleY <= 0f) _maxLookAngleY = 80f; // デフォルト値を設定
 
             // マウスの移動量を取得
-            float rotateX = 0f - Input.GetAxis("Mouse Y") * _sensitivity;
-            float rotateY = Input.GetAxis("Mouse X") * _sensitivity;
-
-            // 累積回転角度を更新
-            _currentX += rotateX;
-            _currentY += rotateY;
-
-            // 回転の制限を適用
-            _currentX = Mathf.Clamp(_currentX, -_maxLookAngleX, _maxLookAngleX);
+            float targetX = _currentX - Input.GetAxis("Mouse Y") * sensitivityX;
+            float targetY = _currentY + Input.GetAxis("Mouse X") * sensitivityY;
+            
+            // Clampを使い上限を設定
+            targetX = Mathf.Clamp(targetX, -_maxLookAngleY, _maxLookAngleY);
+            
+            // SmoothDampでゆっくり補完
+            _currentX = Mathf.SmoothDamp(_currentX, targetX, ref _velocityX, smoothTime);
+            _currentY = Mathf.SmoothDamp(_currentY, targetY, ref _velocityY, smoothTime);
 
             // オイラー角で直接設定することでZ軸の回転を防ぐ
             _cameraPivot.transform.rotation = Quaternion.Euler(_currentX, _currentY, 0f);
