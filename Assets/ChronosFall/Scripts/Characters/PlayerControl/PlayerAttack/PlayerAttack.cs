44f68ab2b3f0349c4e082b4db3054616dd88f064
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-11-14 21:04:02 +0900

    change : 敵に与えるダメージシステムの変更

--- Assets/ChronosFall/Scripts/Characters/PlayerControl/PlayerAttack/PlayerAttack.cs
+++ Assets/ChronosFall/Scripts/Characters/PlayerControl/PlayerAttack/PlayerAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using ChronosFall.Scripts.Interfaces;
 using ChronosFall.Scripts.Systems.Datas;
 using UnityEngine;
@@ -6,21 +7,35 @@ namespace ChronosFall.Scripts.Characters.PlayerControl.PlayerAttack
 {
     public class PlayerAttack : MonoBehaviour
     {
-        public int attackDamage = 20;
-        public ElementType attackType = ElementType.Fire;
-        private const float AttackRange = 5f;
-
+        public int attackDamage = 20; // ダメージ TODO : そのうち動的に変更
+        public ElementType attackType = ElementType.Fire; // プレイヤーの属性 TODO : ここもそのうち動的に変更
+        private const float AttackRange = 5f; // 攻撃距離
+        private const float AttackAngel = 40f; // 攻撃範囲
+        private const float MinStep = 5f; // 最小ステップ角度
+        
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log("Enemy has been attacked by player!");
-                Ray ray = new Ray(transform.position, transform.forward);
-                if (Physics.Raycast(ray, out RaycastHit hit, AttackRange))
+                // 攻撃範囲を最小ステップ角度刻みで割ったときに必要なRay数を計算する
+                int rayCount = Mathf.Max(2, Mathf.CeilToInt(AttackAngel / MinStep) + 1);
+
+                // そのRay数で均等な角度に配置するため、1本ごとの角度間隔を求める
+                float each = AttackAngel / (rayCount - 1);
+                
+                for (int i = 0; i < rayCount; i++)
                 {
-                    if(hit.collider.TryGetComponent(out IEnemyDamageable target))
+                    // 初期を-20°に設定しそこからeachごと足していく
+                    float angle = -AttackAngel / 2f + each * i;
+                    Vector3 dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+                    
+                    if (Physics.Raycast(transform.position + Vector3.up, dir, out RaycastHit hit, AttackRange))
                     {
-                        target.EnemyTakeDamage(attackDamage, attackType);
+                        if (hit.collider.TryGetComponent(out IEnemyDamageable target))
+                        {
+                            target.EnemyTakeDamage(attackDamage, attackType);
+                            Debug.Log($"Enemy has been attacked by player! TARGET : ${target}");
+                        }
                     }
                 }
             }
