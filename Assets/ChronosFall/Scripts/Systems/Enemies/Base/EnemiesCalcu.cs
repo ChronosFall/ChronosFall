8f3b17cffb10b5d9f1fd1628733a3119ea86434f
めだころ <153439300+medakoro0321@users.noreply.github.com>
2025-11-18 23:00:19 +0900

    fix : 0ダメージに固定されたりマイナスダメージになる問題を修正

--- Assets/ChronosFall/Scripts/Systems/Enemies/Base/EnemiesCalcu.cs
+++ Assets/ChronosFall/Scripts/Systems/Enemies/Base/EnemiesCalcu.cs
@@ -46,41 +46,53 @@ namespace ChronosFall.Scripts.Systems.Enemies.Base
             
             // Atk 攻撃者の攻撃力（武器込み）
             _atk = damage;
+            Debug.Log(_atk);
             
             // SkillMult スキル倍率
             _skillMult = _playerData.skillRate;
+            Debug.Log(_skillMult);
             
             // LvFactor Lv1差につき3%変動
             _playerLv = _playerData.level;
             _enemyLv = eData.level;
             _lvFactor = (float)(1 + 0.3 * (_playerLv - _enemyLv));
+            Debug.Log(_lvFactor);
             
             // Def 防御値（被ダメ側）
             _def = eData.def;
+            Debug.Log(_def);
             
             // ElemMult 属性係数 (弱点1.5 / 無効0 / 耐性0.7)
             if (eData.enemyWeakpoint.Contains(elementType)) _elemMult = 1.5f;
             else if (eData.enemyResistancePoint.Contains(elementType)) _elemMult = 0.7f;
+            Debug.Log(_elemMult);
             
             // TODO : BreakMultとCritRollについて更に詳しく考える
             // BreakMult ブレイク補正
             if (UnityEngine.Random.Range(0.0f, 1.0f) <= _playerData.breakChance) _breakMult = _playerData.breakMult;
+            Debug.Log(_breakMult);
             
             // CritRoll CritMultiplier (会心発生で乗算)
             _critRoll = 1;
+            _critMult = 1;
             
             // DMGMod 被ダメ増減 (バフ/デバフ)
             _dmgMod = 1;
             
             // Pierce 防御貫通 (%)
             _pierce = _playerData.pierce;
+            Debug.Log(_pierce);
             
             // RandomOffset ランダム振れ幅
-            const int randomOffsetValue = 5;
+            float randomOffsetValue = 5;
+            randomOffsetValue *= 0.01f;
             _randomOffset = UnityEngine.Random.Range(-randomOffsetValue, randomOffsetValue);
+            Debug.Log(_randomOffset);
             
             // FinalDamage = ((Atk * SkillMult) * LvFactor * (100 / (100 + (Def * (1 - Pierce%)) * dCoef)) * ElemMult * BreakMult * DMGMod * CritFactor) * (1 ± RandomOffset)）
             _finalDamage = ((_atk * _skillMult) * _lvFactor * (100 / (100 + (_def * (1 - _pierce)) * _dCoef)) * _elemMult * _breakMult * _dmgMod * _critMult) * (1 + _randomOffset); 
+            
+            if (_finalDamage <= 0) _finalDamage = 0;
             
             Debug.LogWarning($"FINAL DAMAGE : {_finalDamage}");
             // 四捨五入
