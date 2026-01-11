0f0a235ec15e31606a36770f0a025193e16daeaf
めだころ <153439300+medakoro0321@users.noreply.github.com>
2026-01-11 16:57:06 +0900

    add: 時間操作システムを作成

---
+++ Assets/ChronosFall/Scripts/Characters/Player/TimeControl.cs
@@ -0,0 +1,31 @@
+using ChronosFall.Scripts.Configs;
+using RedGirafeGames.Agamotto.Scripts.Runtime;
+using UnityEngine;
+
+namespace ChronosFall.Scripts.Characters.Player
+{
+    public class TimeControl
+    {
+        // 時間操作
+        private TimeStone _timeStone;
+        private int _timeState;
+        
+        /// <summary>
+        /// 時間操作
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(CharacterInputKey.IsInvertTimeControl ? 1 : 0))
+            {
+                // TODO : メインシステムを考える
+                switch (_timeState)
+                {
+                    case 0:
+                        _timeStone.StopRecording();
+                        _timeState = 1;
+                        break;
+                }
+            }
+        }
+    }
+}
